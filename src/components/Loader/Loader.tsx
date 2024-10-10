05726fb518c8e8b6e91c353cd008e505089b2485
Vitaly Baschenko <vitaly.baschenko@gmail.com>
2024-10-10 20:05:54 +0300

    replacing a crooked spinner

--- src/components/Loader/Loader.tsx
+++ src/components/Loader/Loader.tsx
@@ -1,15 +1,18 @@
-import { InfinitySpin } from 'react-loader-spinner';
+import { ColorRing } from 'react-loader-spinner';
 import s from './Loader.module.css';
 import { FC } from 'react';
 
 export const Loader: FC = () => {
   return (
     <div className={s.container}>
-      <InfinitySpin
+      <ColorRing
         visible={true}
-        width="200"
-        color="#4fa94d"
-        ariaLabel="infinity-spin-loading"
+        height="80"
+        width="80"
+        ariaLabel="color-ring-loading"
+        wrapperStyle={{}}
+        wrapperClass="color-ring-wrapper"
+        colors={['#e15b64', '#f47e60', '#f8b26a', '#abbd81', '#849b87']}
       />
     </div>
   );
